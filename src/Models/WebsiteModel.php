50742ac085aa118b0b888fe1ff71d15953b9be9d
Ramses Moreno <ramses@cuatromedios.com>
2020-05-17 00:34:05 -0500

    Favicons and social share tags updated

--- src/Models/WebsiteModel.php
+++ src/Models/WebsiteModel.php
@@ -2,20 +2,112 @@
 
 namespace Kusikusi\Models;
 
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
-use Mimey\MimeTypes;
-use Kusikusi\Models\EntityModel;
+use Kusikusi\Models\EntityRelation;
 
 class WebsiteModel extends EntityModel
 {
 
     protected $contentFields = [ "title"];
+    protected $propertiesFields = [ "theme_color", "background_color" ];
 
     protected static function boot()
     {
         parent::boot();
+        self::saved(function ($entity) {
+            $faviconRelation = EntityRelation::select('relations.called_entity_id', 'entities.properties->format as format')
+                ->where('caller_entity_id',$entity->id)
+                ->where('kind', EntityRelation::RELATION_MEDIA)
+                ->whereJsonContains('tags', 'favicon')
+                ->leftJoin("entities", function ($join) {
+                    $join->on("relations.called_entity_id", "entities.id");
+                })
+                ->first();
+            if (!is_dir ( public_path("favicons") )) mkdir(public_path("favicons"));
+            if ($faviconRelation) {
+                $id = $faviconRelation->called_entity_id;
+                $path =   $id . '/file.' . $faviconRelation->format;
+                if (Storage::disk('media_original')->exists($path)) {
+                    Image::canvas(192, 192)
+                        ->insert(Image::make(storage_path("media/$path"))->resize(192, 192))
+                        ->save(public_path("favicons/android-chrome-192x192.png"));
+                    Image::canvas(512, 512)
+                        ->insert(Image::make(storage_path("media/$path"))->resize(512, 512))
+                        ->save(public_path("favicons/android-chrome-512x512.png"));
+                    Image::canvas(180, 180, $entity->properties['background_color'])
+                        ->insert(Image::make(storage_path("media/$path"))->resize(148, 148), 'center')
+                        ->save(public_path("favicons/apple-touch-icon.png"));
+                    Image::canvas(16, 16)
+                        ->insert(Image::make(storage_path("media/$path"))->resize(16, 16))
+                        ->save(public_path("favicons/favicon-16x16.png"));
+                    Image::canvas(32, 32)
+                        ->insert(Image::make(storage_path("media/$path"))->resize(32, 32))
+                        ->save(public_path("favicons/favicon-32x32.png"));
+                    Image::canvas(270, 270)
+                        ->insert(Image::make(storage_path("media/$path"))->resize(126, 126), 'top', null, 50)
+                        ->save(public_path("favicons/mstile-150x150.png"));
+                    $favicon = new \PHP_ICO(storage_path("media/$path"), [[48,48]]);
+                    $favicon->save_ico(public_path("favicons/favicon.ico"));
+                    $favicon = new \PHP_ICO(storage_path("media/$path"), [[16,16]]);
+                    $favicon->save_ico(public_path("favicon.ico"));
+                }
+            }
+            $socialRelation = EntityRelation::select('relations.called_entity_id', 'entities.properties->format as format')
+                ->where('caller_entity_id',$entity->id)
+                ->where('kind', EntityRelation::RELATION_MEDIA)
+                ->whereJsonContains('tags', 'social')
+                ->leftJoin("entities", function ($join) {
+                    $join->on("relations.called_entity_id", "entities.id");
+                })
+                ->first();
+            if ($socialRelation) {
+                $id = $socialRelation->called_entity_id;
+                $path =   $id . '/file.' . $socialRelation->format;
+                if (Storage::disk('media_original')->exists($path)) {
+                    Image::canvas(1200, 1200, $entity->properties['background_color'])
+                        ->insert(Image::make(storage_path("media/$path"))->fit(1200, 1200))
+                        ->save(public_path("favicons/social.png"));
+                }
+            }
+            $browserconfig = "<?xml version=\"1.0\" encoding=\"utf-8\"?>
+<browserconfig>
+    <msapplication>
+        <tile>
+            <square150x150logo src=\"/favicons/mstile-150x150.png\"/>
+            <TileColor>".strip_tags($entity->properties['theme_color'])."</TileColor>
+        </tile>
+    </msapplication>
+</browserconfig>
+";
+            $titleContent = EntityContent::select('text')
+                ->where('entity_id', $entity->id)
+                ->where('field', 'title')
+                ->where('lang', config('cms.langs', [''])[0])
+                ->first();
+            $name = $titleContent ? $titleContent->text : '';
+            file_put_contents(public_path("favicons/browserconfig.xml"), $browserconfig);
+            $webmanifest = [
+                "name" => strip_tags($name),
+                "short_name" => strip_tags($name),
+                "icons" => [
+                    [
+                        "src" => "/favicons/android-chrome-192x192.png",
+                        "sizes" => "192x192",
+                        "type" => "image/png"
+                    ],
+                    [
+                        "src" => "/favicons/android-chrome-512x512.png",
+                        "sizes" => "512x512",
+                        "type" => "image/png"
+                    ]
+                ],
+                "theme_color" => $entity->properties['theme_color'],
+                "background_color" => $entity->properties['background_color'],
+                "display" => "standalone"
+            ];
+            file_put_contents(public_path("favicons/site.webmanifest"), json_encode($webmanifest, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+
+        });
     }
 }
